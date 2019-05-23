9859039c10adc0dadd19f461e617353d6de2263f
developer29 <support@d-logic.rs>
2019-05-23 15:00:57 +0200

    Scan button design changed, added top and bottom margins for layouts

--- app/src/main/java/com/dlogic/ufronlinenfcreader/MainActivity.java
+++ app/src/main/java/com/dlogic/ufronlinenfcreader/MainActivity.java
@@ -2,11 +2,13 @@ package com.dlogic.ufronlinenfcreader;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
